bc7a0a18eb32c2e547fe09aba3c9d038c635e9d2
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-17 02:00:29 +0300

    Update Startup.cs

--- src/MapService/Startup.cs
+++ src/MapService/Startup.cs
@@ -70,8 +70,7 @@ public class Startup : BaseApiInfo
             builder
               .AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
           });
     });
 
